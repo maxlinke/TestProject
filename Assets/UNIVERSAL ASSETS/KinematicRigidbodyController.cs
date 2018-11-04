ab176e59f4bf8549b05f8c7842a6e8b4219a11df
maxlinke <maxlinke@noreply.github.com>
2018-11-04 13:48:29 +0100

    Testing rigidbodyconstraints
    >I don't understand how the rotation constraint works when the center of
    mass is not the rigidbody's local origin...
    >But hey, I made the color materials "universal assets" and added a
    kinematic object i can use to test stuff...

---
+++ Assets/UNIVERSAL ASSETS/KinematicRigidbodyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicRigidbodyController : MonoBehaviour {
+
+	[SerializeField] Rigidbody rb;
+	[SerializeField] float moveSpeed;
+	[SerializeField] float turnSpeed;
+
+	void Start () {
+		rb.isKinematic = true;
+	}
+
+	void Reset () {
+		rb = GetComponent<Rigidbody>();
+		moveSpeed = 1f;
+		turnSpeed = 90f;
+	}
+	
+	void FixedUpdate () {
+		Vector3 move = Vector3.zero;
+		if(Input.GetKey(KeyCode.W)) move += Vector3.forward;
+		if(Input.GetKey(KeyCode.S)) move += Vector3.back;
+		if(Input.GetKey(KeyCode.A)) move += Vector3.left;
+		if(Input.GetKey(KeyCode.D)) move += Vector3.right;
+		if(Input.GetKey(KeyCode.E)) move += Vector3.up;
+		if(Input.GetKey(KeyCode.Q)) move += Vector3.down;
+		if(move.magnitude > 1f) move = move.normalized;
+		if(Input.GetKey(KeyCode.LeftShift)) move *= 2f;
+		move = rb.transform.TransformDirection(move) * moveSpeed;
+		rb.MovePosition(rb.transform.position + (move * Time.fixedDeltaTime));
+		float turn = 0f;
+		if(Input.GetKey(KeyCode.LeftArrow)) turn -= 1f;
+		if(Input.GetKey(KeyCode.RightArrow)) turn += 1f;
+		Vector3 newEuler = rb.transform.localEulerAngles + (Vector3.up * turn * turnSpeed * Time.fixedDeltaTime);
+		rb.MoveRotation(Quaternion.Euler(newEuler));
+	}
+}
---
+++ Assets/UNIVERSAL ASSETS/RigidbodyCenterOfMassSetterAndShower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCenterOfMassSetterAndShower : MonoBehaviour {
+
+	[SerializeField] Rigidbody rb;
+	[SerializeField] Transform customCenterOfMass;
+
+	void Start () {
+		if(customCenterOfMass != null) rb.centerOfMass = rb.transform.InverseTransformPoint(customCenterOfMass.position);
+	}
+
+	void Reset () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		Debug.DrawRay(rb.worldCenterOfMass, rb.transform.right, Color.red, Time.fixedDeltaTime, true);
+		Debug.DrawRay(rb.worldCenterOfMass, rb.transform.up, Color.green, Time.fixedDeltaTime, true);
+		Debug.DrawRay(rb.worldCenterOfMass, rb.transform.forward, Color.blue, Time.fixedDeltaTime, true);
+		Debug.DrawRay(rb.worldCenterOfMass, rb.angularVelocity, Color.white, Time.fixedDeltaTime, false);
+	}
+}
