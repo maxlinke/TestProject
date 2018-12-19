92ff36ada9ac66dd081d657ce6eb7d2aca878e06
maxlinke <maxlinke@noreply.github.com>
2018-12-19 17:56:34 +0100

    Shader variant of deformable terrain
    >Instead of deforming the actual terrain (and thus also the collision)
    the terrain now stays and is "deformed" via shader magic
    >Unfortunately shaders aren't magic and so there are some problems
    ->Normals aren't correct at the boundary
    ->The object will be culled even when parts (the extended ones) are
    still onscreen

---
+++ Assets/terraindeformation/DeformableTerrainWithShaders.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformableTerrainWithShaders : MonoBehaviour {
+
+	//assumptions: the object is a simple uv-unwrapped plane that's not rotated (y axis is okay)
+
+	[SerializeField] MeshFilter mf;
+	[SerializeField] MeshRenderer mr;
+	[SerializeField] Collider col;
+	[SerializeField] string texturePropertyName;
+	[SerializeField] string deformationAmountPropertyName;
+	[SerializeField] float deformationAmount;
+	[SerializeField] float deformationRadiusScale;
+
+	int controlTextureSize;
+	MaterialPropertyBlock propBlock;
+	Texture2D controlTexture;
+	Color32[] pixels;
+
+	void Start () {
+		controlTextureSize = Mathf.NextPowerOfTwo(Mathf.FloorToInt(Mathf.Sqrt(mf.sharedMesh.vertexCount) + 0.5f));
+		controlTexture = new Texture2D(controlTextureSize, controlTextureSize);
+		pixels = GetFullColor32Array(controlTextureSize * controlTextureSize, Color.white);
+		controlTexture.SetPixels32(pixels);
+		controlTexture.Apply();
+		propBlock = new MaterialPropertyBlock();
+		propBlock.SetFloat(deformationAmountPropertyName, deformationAmount);
+		propBlock.SetTexture(texturePropertyName, controlTexture);
+	}
+
+	void Reset () {
+		mf = GetComponent<MeshFilter>();
+		mr = GetComponent<MeshRenderer>();
+		col = GetComponent<Collider>();
+		controlTextureSize = 128;
+		deformationAmount = 1;
+		deformationRadiusScale = 1;
+	}
+
+	void Update () {
+		mr.SetPropertyBlock(propBlock);
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		UpdateTexture(collision.collider);
+	}
+
+	void OnCollisionStay (Collision collision) {
+		UpdateTexture(collision.collider);
+	}
+
+	void UpdateTexture (Collider otherCollider) {
+		Vector3 colliderCenter = otherCollider.bounds.center;
+		Vector3 colliderExtents = colliderCenter + (otherCollider.bounds.extents * deformationRadiusScale);
+
+		bool allOkay = true;
+		RaycastHit centerHit, extentsHit;
+		allOkay &= Physics.Raycast(colliderCenter, Vector3.down, out centerHit, Mathf.Infinity, LayerMask.GetMask("DeformableTerrain"));
+		allOkay &= Physics.Raycast(colliderExtents, Vector3.down, out extentsHit, Mathf.Infinity, LayerMask.GetMask("DeformableTerrain"));
+
+		if(allOkay){
+			Vector2 centerUV = centerHit.textureCoord;
+			Vector2 extentsUV = extentsHit.textureCoord;
+			int x, y, w, h;
+			MakePixelRectangleData(centerUV, extentsUV, out x, out y, out w, out h);
+			if(w < 1) w = 1;
+			if(h < 1) h = 1;
+			FillRectangle(ref pixels, controlTextureSize, controlTextureSize, x, y, w, h, Color.black);
+			controlTexture.SetPixels32(pixels);
+			controlTexture.Apply();
+		}
+	}
+
+	void MakePixelRectangleData (Vector2 centerUV, Vector2 extentsUV, out int x, out int y, out int w, out int h) {
+		int cx = Mathf.FloorToInt(centerUV.x * controlTextureSize);
+		int cy = Mathf.FloorToInt(centerUV.y * controlTextureSize);
+		int ex = Mathf.FloorToInt(extentsUV.x * controlTextureSize);
+		int ey = Mathf.FloorToInt(extentsUV.y * controlTextureSize);
+		int dx = ex - cx;
+		int dy = ey - cy;
+		int minX = Mathf.Min(cx - dx, cx + dx);
+		int minY = Mathf.Min(cy - dy, cy + dy);
+		int maxX = Mathf.Max(cx - dx, cx + dx);
+		int maxY = Mathf.Max(cy - dy, cy + dy);
+		x = minX;
+		y = minY;
+		w = maxX - minX;
+		h = maxY - minY;
+	}
+
+	Color32 ToColor32 (Color color) {
+		byte r = (byte)(255 * color.r);
+		byte g = (byte)(255 * color.g);
+		byte b = (byte)(255 * color.b);
+		byte a = (byte)(255 * color.a);
+		return new Color32(r, g, b, a);
+	}
+
+	Color32[] GetFullColor32Array (int size, Color color) {
+		Color32 col32 = ToColor32(color);
+		Color32[] output = new Color32[size];
+		for(int i=0; i<size; i++){
+			output[i] = col32;
+		}
+		return output;
+	}
+
+	void FillRectangle (ref Color32[] colors, int width, int height, int sx, int sy, int dx, int dy, Color newColor) {
+		Color32 col32 = ToColor32(newColor);
+		if((width - sx) < dx) dx = width - sx;
+		if((height - sy) < dy) dy = height - sy;
+		for(int i=0; i<dy; i++){
+			for(int j=0; j<dx; j++){
+				int y = i + sy;
+				int x = j + sx;
+				int pos = (y * width) + x;
+				colors[pos] = col32;
+			}
+		}
+	}
+	
+}
---
+++ Assets/terraindeformation/X_DeformationSnow.shader
@@ -0,0 +1,55 @@
+﻿Shader "Custom/Experimental/X_DeformationSnow" {
+
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		[NoScaleOffset] _TopTex ("Top Texture", 2D) = "white" {}
+		_TopTexTiling ("Top Texture Tiling", Float) = 1.0
+		[NoScaleOffset] _BottomTex ("Bottom Texture", 2D) = "grey" {}
+		_BottomTexTiling ("Bottom Texture Tiling", Float) = 1.0
+		[PerRendererData] _DeformationTex ("Deformation Control Texture", 2D) = "black" {}
+		[PerRendererData] _DeformationAmount ("Deformation Amount", Float) = 1.0
+	}
+
+	SubShader {
+
+		Tags { "RenderType"="Opaque" }
+
+		CGPROGRAM
+		#pragma surface surf Lambert addshadow vertex:vert
+
+		fixed4 _Color;
+		sampler2D _TopTex;
+		sampler2D _BottomTex;
+		sampler2D _DeformationTex;
+		float _DeformationAmount;
+		float _TopTexTiling;
+		float _BottomTexTiling;
+
+		struct Input {
+			float level;
+			float4 coords;
+		};
+
+		void vert (inout appdata_full v, out Input o) {
+			UNITY_INITIALIZE_OUTPUT(Input, o);
+			o.level = tex2Dlod(_DeformationTex, v.texcoord).r;
+			v.vertex.y += o.level * _DeformationAmount;
+			float2 wp = mul(unity_ObjectToWorld, v.vertex).xz;
+			o.coords = float4(0,0,0,0);
+			o.coords.xy += wp * _TopTexTiling;
+			o.coords.zw += wp * _BottomTexTiling;
+		}		
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			fixed4 top = tex2D(_TopTex, IN.coords.xy);
+			fixed4 bottom = tex2D(_BottomTex, IN.coords.zw);
+			fixed4 c = lerp(bottom, top, IN.level) * _Color;
+			o.Albedo = c.rgb;
+			o.Alpha = c.a;
+		}
+
+		ENDCG
+	}
+
+	FallBack "Diffuse"
+}
