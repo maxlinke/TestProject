e8adb96aa65d63bbb49fc3574cbcf08a041048fa
maxlinke <maxlinke@noreply.github.com>
2018-12-24 13:41:08 +0100

    Short experiment with seeding a random number generator
    >Just a little comparison between System.Random and UnityEngine.Random

---
+++ Assets/seeded randomness/SeededRandomness.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeededRandomness : MonoBehaviour {
+
+	[SerializeField] bool seedFromString;
+	[SerializeField] string stringSeed;
+	[SerializeField] int seed;
+
+	[SerializeField] KeyCode keySysRng;
+	[SerializeField] KeyCode keyUnityRng;
+
+	System.Random sysRng;
+
+	void Start () {
+		if(seedFromString && (stringSeed.Length > 0)){
+			seed = stringSeed.GetHashCode();
+		}
+		sysRng = new System.Random(seed);
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(keySysRng)){
+			Debug.Log("sys : " + ((float)sysRng.NextDouble()).ToString());
+		}
+		if(Input.GetKeyDown(keyUnityRng)){
+			Debug.Log("unity : " + UnityEngine.Random.value.ToString());
+		}
+	}
+}
