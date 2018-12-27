b696cd310e009285204d0daeac314d522b7897e0
Max Linke <maxlinke@users.noreply.github.com>
2018-12-27 18:28:43 +0100

    Mesh cutting works now
    >Added MeshCutter that can cut meshes using spherecolliders
    >Put the appropriate testing code into the MeshGraphTester...

---
+++ Assets/mesh graphs/MeshCutter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCutter {
+
+	public static Mesh RemoveAllVerticesInside (Mesh mesh, Transform meshTransform, SphereCollider[] colliders) {
+		return RemoveVerts(mesh, meshTransform, colliders, true);
+	}
+
+	public static Mesh RemoveAllVerticesOutside (Mesh mesh, Transform meshTransform, SphereCollider[] colliders) {
+		return RemoveVerts(mesh, meshTransform, colliders, false);
+	}
+
+	private static Mesh RemoveVerts (Mesh mesh, Transform meshTransform, SphereCollider[] colliders, bool removeInside) {
+		MeshGraph mg = MeshGraph.FromMesh(mesh);
+		MeshGraph.GraphVertex[] vertsInMesh = mg.GetVertices();
+		List<MeshGraph.GraphVertex> vertsToRemove = new List<MeshGraph.GraphVertex>();
+		for(int i=0; i<vertsInMesh.Length; i++){
+			Vector3 worldPos = meshTransform.TransformPoint(vertsInMesh[i].position);
+			for(int j=0; j<colliders.Length; j++){
+				Vector3 lossyScale = colliders[j].transform.lossyScale;
+				float transformScale = Mathf.Max(Mathf.Max(Mathf.Abs(lossyScale.x), Mathf.Abs(lossyScale.y)), Mathf.Abs(lossyScale.z));
+				float actualRadius = colliders[j].radius * transformScale;
+				bool vertexIsInside = ((worldPos - colliders[j].transform.position).sqrMagnitude < (actualRadius * actualRadius));
+				if(vertexIsInside && removeInside){
+					vertsToRemove.Add(vertsInMesh[i]);
+					break;
+				}else if(!vertexIsInside && !removeInside){
+					vertsToRemove.Add(vertsInMesh[i]);
+					break;
+				}
+			}
+		}
+		for(int i=0; i<vertsToRemove.Count; i++){
+			mg.RemoveVertex(vertsToRemove[i]);
+		}
+		return mg.ToMesh();
+	}
+
+}
--- Assets/mesh graphs/MeshGraphTester.cs
+++ Assets/mesh graphs/MeshGraphTester.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class MeshGraphTester : MonoBehaviour {
 
+	public enum CutoutMode {
+		REMOVEINSIDE,
+		REMOVEOUTSIDE
+	}
+
 	[SerializeField] Mesh mesh;
 	[SerializeField] MeshFilter mf;
 	[SerializeField] MeshCollider mc;
+
+	[Header("Cutting stuff")]
+	[SerializeField] KeyCode cutoutKey;
+	[SerializeField] CutoutMode cutoutMode;
+	[SerializeField] SphereCollider[] colliders;
 
 	void Start () {
 		MeshGraph mg = MeshGraph.FromMesh(mesh);
@@ -15,9 +25,26 @@ public class MeshGraphTester : MonoBehaviour {
 		if(mc != null) mc.sharedMesh = reconstructed;
 	}
 
+	void Update () {
+		if(Input.GetKeyDown(cutoutKey)){
+			Mesh modified;
+			switch(cutoutMode){
+				case CutoutMode.REMOVEINSIDE: modified = MeshCutter.RemoveAllVerticesInside(mesh, this.transform, colliders); break;
+				case CutoutMode.REMOVEOUTSIDE: modified = MeshCutter.RemoveAllVerticesOutside(mesh, this.transform, colliders); break;
+				default: throw new UnityException("Unknown CutoutMode \"" + cutoutMode.ToString() + "\"");
+			}
+			if(mf != null) mf.sharedMesh = modified;
+			if(mc != null) mc.sharedMesh = modified;
+		}
+	}
+
 	void Reset () {
 		mesh = null;
 		mf = GetComponent<MeshFilter>();
 		mc = GetComponent<MeshCollider>();
+
+		cutoutKey = KeyCode.C;
+		cutoutMode = CutoutMode.REMOVEINSIDE;
+		colliders = new SphereCollider[0];
 	}
 }
