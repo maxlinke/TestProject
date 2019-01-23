6fadd5baf715cc21c40cfa4dd94ae2ab11c245e8
Max Linke <maxlinke@users.noreply.github.com>
2019-01-23 15:47:15 +0100

    Event Callbacks and UI Shaders
    > Added object that logs event callbacks (pointer up/down/...)
    > Added a UI shader that reflects a cubemap and looks nice n shit

---
+++ Assets/ui event callbacks/EventCallbackTester.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventCallbackTester : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler {
+
+	[SerializeField] bool logEnter;
+	[SerializeField] bool logExit;
+	[SerializeField] bool logDown;
+	[SerializeField] bool logUp;
+	[SerializeField] bool logClick;
+
+	void Log (string msg) {
+		Debug.Log(gameObject.name + " : " + msg);
+	}
+
+	public void OnPointerEnter (PointerEventData ped) {
+		if(logEnter) Log("enter");
+	}
+
+	public void OnPointerExit (PointerEventData ped) {
+		if(logExit) Log("exit");
+	}
+
+	public void OnPointerDown (PointerEventData ped) {
+		if(logDown) Log("down");
+	}
+
+	public void OnPointerUp (PointerEventData ped) {
+		if(logUp) Log("up");
+	}
+
+	public void OnPointerClick (PointerEventData ped) {
+		if(logClick) Log("click");
+	}
+
+}
