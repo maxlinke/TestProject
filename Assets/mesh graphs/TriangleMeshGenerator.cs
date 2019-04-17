123650fdaea95278e94aa364d2a7437ff374af67
Max Linke <maxlinke@users.noreply.github.com>
2019-04-17 04:05:42 +0200

    meshes!

--- Assets/mesh graphs/TriangleMeshGenerator.cs
+++ Assets/mesh graphs/TriangleMeshGenerator.cs
@@ -9,6 +9,7 @@ using UnityEditor;
 [ExecuteAlways]
 public class TriangleMeshGenerator : MonoBehaviour {
 
+    [SerializeField] MeshFilter mf;
     [SerializeField] GameObject selectObject;
     [SerializeField] float selectRadius;
     [SerializeField] float lineLength;
@@ -21,6 +22,16 @@ public class TriangleMeshGenerator : MonoBehaviour {
     float mediumGizmosSize => 0.5f * debugGizmoSize;
     float smallGizmoSize => 0.25f * debugGizmoSize;
     float miniGizmoSize => 0.05f * debugGizmoSize;
+
+    [ContextMenu(nameof(GenerateMesh))]
+    void GenerateMesh () {
+        var graph = new TriMeshGraph(this);
+        var selectedPoints = GetPointsInsideRadius(selectObject.transform.position, selectRadius);
+        foreach(var point in selectedPoints){
+            graph.AddVertex(point);
+        }
+        mf.sharedMesh = graph.ToMesh(TriMeshGraph.UVMode.OBJECTNORMALIZED);
+    }
 
     void OnDrawGizmos () {
         // DrawDebugGridAndAxes();
@@ -85,11 +96,11 @@ public class TriangleMeshGenerator : MonoBehaviour {
         }
     }
 
-    TriMeshPoint WorldXYZtoNearestABC (Vector3 worldPoint) {
+    public TriMeshPoint WorldXYZtoNearestABC (Vector3 worldPoint) {
         return LocalXYZtoNearestABC(transform.InverseTransformPoint(worldPoint));
     }
 
-    TriMeshPoint LocalXYZtoNearestABC (Vector3 localPoint) {
+    public TriMeshPoint LocalXYZtoNearestABC (Vector3 localPoint) {
         var normedLocalPoint =  localPoint /  lineLength;
         int a = Mathf.FloorToInt(Vector3.Dot(normedLocalPoint, aAxis) + 0.5f);
         int b = Mathf.FloorToInt(Vector3.Dot(normedLocalPoint, bAxis) + 0.5f);
@@ -108,17 +119,17 @@ public class TriangleMeshGenerator : MonoBehaviour {
         return closest;
     }
 
-    Vector3 ABCtoWorldXYZ (TriMeshPoint triPoint) {
+    public Vector3 ABCtoWorldXYZ (TriMeshPoint triPoint) {
         return transform.TransformPoint(ABCtoLocalXYZ(triPoint));
     }
 
-    Vector3 ABCtoLocalXYZ (TriMeshPoint triPoint) {
+    public Vector3 ABCtoLocalXYZ (TriMeshPoint triPoint) {
         float z = ((triPoint.b * aAxis.x) - (bAxis.x * triPoint.a)) / ((bAxis.z * aAxis.x) - (aAxis.z * bAxis.x));
         float x = (triPoint.a - (z * aAxis.z)) / aAxis.x;
         return new Vector3(x, 0, z) * lineLength;
     }
 
-    List<TriMeshPoint> GetPointsInsideRadius (Vector3 worldCenterPoint, float worldRadius) {
+    public List<TriMeshPoint> GetPointsInsideRadius (Vector3 worldCenterPoint, float worldRadius) {
         var output = new List<TriMeshPoint>();
         float sqrWorldRadius = worldRadius * worldRadius;
         var triCenter = WorldXYZtoNearestABC(worldCenterPoint);
@@ -182,11 +193,19 @@ public class TriangleMeshGenerator : MonoBehaviour {
         }
     }
 
-    struct TriMeshPoint {
+    public struct TriMeshPoint {
 
         public readonly int a;
         public readonly int b;
         public int c => -(a+b);
+
+        public static bool operator == (TriMeshPoint point1, TriMeshPoint point2) {
+            return point1.Equals(point2);
+        }
+
+        public static bool operator != (TriMeshPoint point1, TriMeshPoint point2) {
+            return !point1.Equals(point2);
+        }
 
         public TriMeshPoint (int a, int b) {
             this.a = a;
@@ -200,6 +219,170 @@ public class TriangleMeshGenerator : MonoBehaviour {
             if(sum != 0){
                 throw new System.Exception($"Error, {nameof(TriMeshPoint)} indices must have a sum of 0. Indices: {a}, {b}, {c}, Sum: {sum}.");
             }
+        }
+
+        public bool Neighbors (TriMeshPoint other) {
+            if(other == this){
+                return false;
+            }
+            int aDelta = Mathf.Abs(other.a - this.a);
+            int bDelta = Mathf.Abs(other.b - this.b);
+            int cDelta = Mathf.Abs(other.c - this.c);
+            if(aDelta > 1 || bDelta > 1 || cDelta > 1){
+                return false;
+            }
+            return ((aDelta + bDelta + cDelta) == 2);
+        }
+
+        public override bool Equals (object obj) {
+            if(obj is TriMeshPoint other){
+                return ((other.a == this.a) && (other.b == this.b));
+            }
+            return false;
+        }
+
+        public override int GetHashCode () {
+            return 2179 * a + 3853 * b + 4967 * c;  //just some prime numbers
+        }
+
+    }
+
+    class TriMeshGraph {
+
+        //TODO use normal mesh graph to implement some sort of "patch holes" thingy...
+        //or maybe not. here i pretty much know that any vertex should have 6 neighbors unless it is at an edge...
+
+        public enum UVMode {
+            WORLDCOORDS,
+            OBJECTNORMALIZED
+        }
+
+        public class TriMeshGraphVertex {
+
+            public readonly TriMeshPoint point;
+            private readonly List<TriMeshGraphVertex> connected;
+
+            public TriMeshGraphVertex (TriMeshPoint point) {
+                this.point = point;
+                connected = new List<TriMeshGraphVertex>();
+            }
+
+            public void ClearConnections () {
+                connected.Clear();
+            }
+
+            public void AddConnected (TriMeshGraphVertex other) {
+                if(IsConnectedTo(other)){
+                    Debug.LogError("duplicate connection, aborting");
+                    return;
+                }
+                connected.Add(other);
+            }
+
+            public bool IsConnectedTo (TriMeshGraphVertex other) {
+                return connected.Contains(other);
+            }
+
+        }
+
+        private readonly TriangleMeshGenerator generator;
+        private readonly List<TriMeshGraphVertex> vertices;
+
+        public TriMeshGraph (TriangleMeshGenerator generator) {
+            this.generator = generator;
+            vertices = new List<TriMeshGraphVertex>();
+        }
+
+        public TriMeshGraphVertex AddVertex (TriMeshPoint point) {
+            if(HasVertex(point, out _)){
+                Debug.LogError("duplicate vertices not allowed!");
+                return null;
+            }
+            var newVert = new TriMeshGraphVertex(point);
+            vertices.Add(newVert);
+            return newVert;
+        }
+
+        public bool HasVertex (TriMeshPoint point, out TriMeshGraphVertex outputVert) {
+            foreach(var vert in vertices){
+                if(vert.point == point){
+                    outputVert = vert;
+                    return true;
+                }
+            }
+            outputVert = null;
+            return false;
+        }
+
+        void RegenerateConnections () {
+            foreach(var vert in vertices){
+                vert.ClearConnections();
+            }
+            for(int i=0; i<vertices.Count; i++){
+                var vert = vertices[i];
+                for(int j=i+1; j<vertices.Count; j++){
+                    var otherVert = vertices[j];
+                    if(!vert.IsConnectedTo(otherVert) && vert.point.Neighbors(otherVert.point)){
+                        vert.AddConnected(otherVert);
+                        otherVert.AddConnected(vert);
+                    }
+                }
+            }
+        }
+
+        public Mesh ToMesh (UVMode uvMode) {
+            RegenerateConnections();
+            Vector3[] meshVertices = new Vector3[vertices.Count];
+            Vector2[] meshTexcoords = new Vector2[vertices.Count];
+            var triangleList = new List<(int, int, int)>();
+            for(int i=0; i<vertices.Count; i++){
+                var vert = vertices[i];
+                meshVertices[i] = generator.ABCtoLocalXYZ(vert.point);      //maybe start off with local (for tex coords) then before the end transform them into world space?
+                for(int j=i+1; j<vertices.Count; j++){
+                    var otherVert = vertices[j];
+                    if(vert.IsConnectedTo(otherVert)){
+                        for(int k=i+2; k<vertices.Count; k++){
+                            var otherOtherVert = vertices[k];
+                            if(vert.IsConnectedTo(otherOtherVert) && otherVert.IsConnectedTo(otherOtherVert)){
+                                triangleList.Add((i, j, k));
+                            }
+                        }
+                    }
+                }
+            }
+            int[] meshTriangles = new int[3 * triangleList.Count];
+            for(int i=0; i<triangleList.Count; i++){
+                int index1 = triangleList[i].Item1;
+                int index2 = triangleList[i].Item2;
+                int index3 = triangleList[i].Item3;
+                Vector3 p1 = meshVertices[index1];
+                Vector3 p2 = meshVertices[index2];
+                Vector3 p3 = meshVertices[index3];
+                float orientation = (p2.z - p1.z) * (p3.x - p2.x) - (p2.x - p1.x) * (p3.z - p2.z);
+                int i3 = i * 3;
+                if(orientation > 0){
+                    meshTriangles[i3 + 0] = index1;
+                    meshTriangles[i3 + 1] = index2;
+                    meshTriangles[i3 + 2] = index3;
+                }else{
+                    meshTriangles[i3 + 0] = index1;
+                    meshTriangles[i3 + 1] = index3;
+                    meshTriangles[i3 + 2] = index2;
+                }
+            }
+            for(int i=0; i<meshVertices.Length; i++){
+                meshVertices[i] = generator.transform.TransformPoint(meshVertices[i]);
+            }
+            Mesh outputMesh = new Mesh();
+            outputMesh.vertices = meshVertices;
+            outputMesh.uv = meshTexcoords;
+            outputMesh.triangles = meshTriangles;
+            outputMesh.normals = new Vector3[vertices.Count];
+            outputMesh.tangents = new Vector4[vertices.Count];
+            outputMesh.RecalculateBounds();
+            outputMesh.RecalculateNormals();
+            outputMesh.RecalculateTangents();
+            return outputMesh;
         }
 
     }
