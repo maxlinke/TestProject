17f4b4c22e3927161d738b69b626483fd214ff39
Max Linke <maxlinke@users.noreply.github.com>
2020-04-21 22:23:53 +0200

    generalized the nextTFromTDistance a bit
    > both euclidian and "bezier" distance are now possible
    > bezier only works forward though...

--- Assets/bsplineobjectplacement/BSplineObjectPlacer.cs
+++ Assets/bsplineobjectplacement/BSplineObjectPlacer.cs
@@ -14,12 +14,20 @@ public class BSplineObjectPlacer : MonoBehaviour {
 
     [Header("Settings")]
     [SerializeField] GroundMode groundMode;
+    [SerializeField] Collider groundCollider;
+    [SerializeField] PlacementMode placementMode;
+    [SerializeField] bool flipSides;
+    [SerializeField] Vector2 placementRandomness;
 
     [Header("Debug")]
     [SerializeField] float testStartT;
     [SerializeField] float testDistance;
+    [SerializeField] bool testEuclidian;
     [SerializeField] float testDistanceOutput;
+    [SerializeField] float testDistanceOuptutDeviation;
     [SerializeField, Range(0, 20)] int testIterations;
+    [SerializeField, Range(0, 20)] int testPrecision;
+    [SerializeField] bool ASDF;
 
     public bool setUp => (handle1 != null && handle2 != null && controlHandle != null);
 
@@ -29,15 +37,17 @@ public class BSplineObjectPlacer : MonoBehaviour {
         SNAP_AND_ALIGN
     }
 
-    void Start () {
-        
-    }
-
-    void Update () {
-        
+    enum PlacementMode {
+        CENTER,
+        SIDE,
+        CORNER
     }
 
     void OnDrawGizmos () {
+        if(ASDF){
+            Debug.Log(BezierDistanceEstimate(0.5642f, 0.3123f));
+            ASDF = false;
+        }
         if(!setUp || !drawGizmos){
             return;
         }
@@ -52,22 +62,22 @@ public class BSplineObjectPlacer : MonoBehaviour {
         }
         Gizmos.color = Color.white;
         Vector3 s = BezierPoint(testStartT);
-        float nT = NextTFromDistance(testStartT, testDistance, testIterations);
-        Vector3 t = BezierPoint(nT);
+        Vector3 t;
+        if(testEuclidian){
+            float nT = NextTFromEuclidianDistance(testStartT, testDistance, testIterations);
+            t = BezierPoint(nT);
+            testDistanceOutput = (s - t).magnitude;
+        }else{
+            float nT = NextTFromBezierDistance(testStartT, testDistance, testPrecision, testIterations);
+            t = BezierPoint(nT);
+            testDistanceOutput = BezierDistanceEstimate(testStartT, nT);
+        }
+        testDistanceOuptutDeviation = testDistance - testDistanceOutput;
         Gizmos.DrawSphere(s, gizmoSize);
         Gizmos.DrawSphere(t, gizmoSize);
-        testDistanceOutput = (s - t).magnitude;
 
         // float l = BezierLengthEstimate();
-        // float t = 0f;
-        // // while(t<1f){
-        // //     Gizmos.DrawCube(BezierPoint(t), gizmoSize * Vector3.one);
-        // //     float nextT = t;
-        // //     float lastDelta = 0;
-        // //     for(int i=0; i<4; i++){
 
-        // //     }
-        // // }
         Gizmos.color = colorChache;
     }
 
@@ -87,11 +97,11 @@ public class BSplineObjectPlacer : MonoBehaviour {
         return (2f * iT * (pC - p1)) + (2f * t * (p2 - pC));
     }
 
-    float BezierLengthEstimate (int steps = 100) {
+    float BezierDistanceEstimate (float startT, float endT, int steps = 100) {
         float total = 0f;
-        Vector3 last = BezierPoint(0f);
+        Vector3 last = BezierPoint(startT);
         for(int i=0; i<steps; i++){
-            float t = (float)(i+1) / steps;
+            float t = Mathf.Lerp(startT, endT, (float)(i+1) / steps);
             Vector3 current = BezierPoint(t);
             total += (current - last).magnitude;
             last = current;
@@ -99,48 +109,68 @@ public class BSplineObjectPlacer : MonoBehaviour {
         return total;
     }
 
-    float NextTFromDistance (float t, float desiredDistance, int iterations = 16, bool dontCalculateLength = false, float inputLength = 0f) {
-        if(desiredDistance == 0 || float.IsNaN(desiredDistance) || float.IsInfinity(desiredDistance)){
-            return t;
-        }
-        float length = (dontCalculateLength ? inputLength : BezierLengthEstimate());
-        if(length == 0 || float.IsInfinity(length) || float.IsNaN(length)){
-            return t;
-        }
+    float NextTFromEuclidianDistance (float startT, float desiredDistance, int iterations = 16, bool dontCalculateLength = false, float inputLength = 0f) {
         float desiredSqrDistance = desiredDistance * desiredDistance;
-        Vector3 startPoint = BezierPoint(t);
-        float lastSqrDistanceDelta = SqrDistanceDelta(t);
-        float closestT = t;
-        float closestTAbsSqrDistanceDelta = Mathf.Abs(lastSqrDistanceDelta);
+        Vector3 startPoint = BezierPoint(startT);
+        return NextTFromDistance(
+            getDistDelta: (testT) => (desiredSqrDistance - (BezierPoint(testT) - startPoint).sqrMagnitude), 
+            startT: startT, 
+            desiredDistance: desiredDistance, 
+            iterations: iterations, 
+            dontCalculateLength: dontCalculateLength, 
+            inputLength: inputLength
+        );
+    }
+
+    float NextTFromBezierDistance (float startT, float desiredDistance, int precision, int iterations = 16, bool dontCalculateLength = false, float inputLength = 0f) {
+        Vector3 startPoint = BezierPoint(startT);
+        return NextTFromDistance(
+            getDistDelta: (testT) => (desiredDistance - BezierDistanceEstimate(startT, testT, precision)),
+            startT: startT, 
+            desiredDistance: desiredDistance, 
+            iterations: iterations, 
+            dontCalculateLength: dontCalculateLength, 
+            inputLength: inputLength
+        );
+    }
+
+    float NextTFromDistance (System.Func<float, float> getDistDelta, float startT, float desiredDistance, int iterations, bool dontCalculateLength, float inputLength) {
+        if(desiredDistance == 0 || float.IsNaN(desiredDistance) || float.IsInfinity(desiredDistance)){
+            return startT;
+        }
+        float length = (dontCalculateLength ? inputLength : BezierDistanceEstimate(0f, 1f));
+        if(length == 0 || float.IsInfinity(length) || float.IsNaN(length)){
+            return startT;
+        }
+        float lastDistDelta = getDistDelta(startT);
+        float closestT = startT;
+        float closestTAbsDistDelta = Mathf.Abs(lastDistDelta);
         float delta = desiredDistance / length;     // just a solid(ish) guess. this is where the direction comes from as desiredDistance can be positive or negative...
         float deltaMultiplier = 2f;
 
         for(int i=0; i<iterations; i++){
-            t += delta;
-            float sqrDistanceDelta = SqrDistanceDelta(t);
-            if(sqrDistanceDelta == 0){
-                closestT = t;
+            startT += delta;
+            float currentDistDelta = getDistDelta(startT);
+            if(currentDistDelta == 0){
+                closestT = startT;
                 break;
             }
-            if(Mathf.Sign(sqrDistanceDelta) != Mathf.Sign(lastSqrDistanceDelta)){   // over-/undershoot
+            if(Mathf.Sign(currentDistDelta) != Mathf.Sign(lastDistDelta)){   // over-/undershoot
                 delta *= -1f;               // flip direction
                 deltaMultiplier = 0.5f;     // from now on we're inching closer every iteration
             }
-            float absSqrDistanceDelta = Mathf.Abs(sqrDistanceDelta);
-            if(absSqrDistanceDelta < closestTAbsSqrDistanceDelta){
-                closestT = t;
-                closestTAbsSqrDistanceDelta = absSqrDistanceDelta;
+            float absDistDelta = Mathf.Abs(currentDistDelta);
+            if(absDistDelta < closestTAbsDistDelta){
+                closestT = startT;
+                closestTAbsDistDelta = absDistDelta;
             }
             delta *= deltaMultiplier;
-            lastSqrDistanceDelta = sqrDistanceDelta;
+            lastDistDelta = currentDistDelta;
         }
 
         return closestT;
 
-        // positive: not far enough, negative: too far (kinda)
-        float SqrDistanceDelta (float testT) {
-            return desiredSqrDistance - (BezierPoint(testT) - startPoint).sqrMagnitude;
-        }
+        
     }
 
 }
