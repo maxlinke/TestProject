80f84dea92be9d13c351212a37722c6ac6db8f32
Max Linke <maxlinke@users.noreply.github.com>
2020-05-15 23:30:56 +0200

    lots of thinking, so i'll commit almost nothing
    > need to implement a closestpoint function for my splines

--- Assets/bsplineobjectplacement/bird boids/BirdBoid.cs
+++ Assets/bsplineobjectplacement/bird boids/BirdBoid.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class BirdBoid : MonoBehaviour {
 
+    static List<BirdBoid> allBirdBoids;
+
+    void OnEnable () {
+        if(allBirdBoids == null){
+            allBirdBoids = new List<BirdBoid>();
+        }
+    }
+
+    // settings for this boird
+    // placer
+    // init with list of ALL others (including this one)
+    // static list of all boirds
+
+    // collision avoidance? select colliders only... and only occasionally
+
+    // singleton boird util thingy
+
+    // terrain for ground, height above for lower flight ceiling
+
     void Start () {
         
     }
---
+++ Assets/bsplineobjectplacement/bird boids/BirdBoidSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Bird Boid Settings", fileName = "New Bird Boid Settings")]
+public class BirdBoidSettings : ScriptableObject {
+
+    [SerializeField] TerrainCollider ground;
+    [SerializeField] float upperFlightCeiling;
+
+	
+}
