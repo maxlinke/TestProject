0001ab20324863fb154ca8e7d626c461d16a4078
Max Linke <maxlinke@users.noreply.github.com>
2020-05-15 17:46:46 +0200

    unified the random and structured mesh pools into one mesh pool
    > also removed the extra scriptable objects. it's all in one pool now. customeditor coming right up...
    > oh, and the behavior is identical, so that's good...

--- Assets/bsplineobjectplacement/MeshPool.cs
+++ Assets/bsplineobjectplacement/MeshPool.cs
@@ -4,10 +4,141 @@ using UnityEngine;
 
 namespace SplineTools{
 
-    public abstract class MeshPool : ObjectPool {
+    [CreateAssetMenu(menuName = "Spline Tools/Mesh Pool", fileName = "New MeshPool")]
+    public class MeshPool : ObjectPool {
 
         public const string NO_BOXCOLLS_WARNING = "No BoxCollider on object, even though it is marked to be used!";
         public const string TOO_MANY_BOXCOLLS_WARNING = "Multiple BoxColliders on object, this isn't supported!";
+
+        private const int MAX_DUPLICATE_AVOIDANCE_ITERATIONS = 32;
+
+        [SerializeField] OutputType outputType;
+        [SerializeField] RandomRepetitionType randomRepetitionType;
+        [SerializeField] MeshPoolObject[] objects;
+
+        public override int ObjectCount => objects.Length;
+
+        List<MeshPoolObject> occurenceList;
+        int nextIndex;
+        MeshPoolObject lastPO;
+        Material lastMat;
+
+        public enum OutputType {
+            ORDERED,
+            RANDOM
+        }
+
+        public enum RandomRepetitionType {
+            ALLOW,
+            AVOID_DUPLICATE_OBJECT,
+            AVOID_DUPLICATE_APPEARANCE
+        }
+
+        protected override void Init () {
+            base.Init();
+            nextIndex = 0;
+            lastPO = null;
+            lastMat = null;
+            FillOccurenceList();
+        }
+
+        protected override void DeInit () {
+            base.DeInit();
+            occurenceList.Clear();
+        }
+
+        protected override SplineObject GetNext (Vector3 measureAxis, System.Random rng) {
+            if(objects == null || objects.Length <= 0){
+                return null;
+            }
+            MeshPoolObject po;
+            Material mat;
+            switch(outputType){
+                case OutputType.ORDERED:
+                    po = objects[nextIndex];
+                    mat = po.RandomMaterial(rng);
+                    break;
+                case OutputType.RANDOM:
+                    po = null;
+                    mat = null;
+                    for(int i=0; i<MAX_DUPLICATE_AVOIDANCE_ITERATIONS; i++){
+                        var selectIndex = (rng != null) ? rng.Next(0, occurenceList.Count) : Random.Range(0, occurenceList.Count);
+                        var validSelection = false;
+                        po = occurenceList[selectIndex];
+                        mat = po.RandomMaterial(rng);
+                        var samePO = po == lastPO;
+                        var sameMat = mat == lastMat;
+                        switch(randomRepetitionType){
+                            case RandomRepetitionType.ALLOW:
+                                validSelection = true;
+                                break;
+                            case RandomRepetitionType.AVOID_DUPLICATE_APPEARANCE:
+                                validSelection = !(samePO && sameMat);
+                                break;
+                            case RandomRepetitionType.AVOID_DUPLICATE_OBJECT:
+                                validSelection = !samePO;
+                                break;
+                            default:
+                                Debug.LogError($"Unknown {nameof(RandomRepetitionType)} \"{randomRepetitionType}\"!");
+                                validSelection = true;
+                                break;
+                        }
+                        if(validSelection){
+                            break;
+                        }
+                    }
+                    break;
+                default:
+                    Debug.LogError($"Unknown {typeof(OutputType)} \"{outputType}\"!");
+                    return null;
+            }
+            var output = InstantiatePrefabAndMeasureSize(po.Prefab, mat, measureAxis, po.UseBoxColliderForSpacing);
+            nextIndex = (nextIndex + 1) % ObjectCount;
+            lastPO = po;
+            lastMat = mat;
+            return output;
+        }
+
+        void FillOccurenceList () {
+            if(occurenceList != null && occurenceList.Count > 0){
+                Debug.LogWarning("List was called to be filled, even though it wasn't empty. Code will run fine but this shouldn't happen!");
+                return;
+            }
+            if(occurenceList == null){
+                occurenceList = new List<MeshPoolObject>();
+            }
+            List<string> nullPrefabs = new List<string>();
+            for(int i=0; i<objects.Length; i++){
+                if(objects[i].Prefab == null){
+                    nullPrefabs.Add($"Entry #{i}");
+                }else{
+                    for(int j=0; j<objects[i].Occurences; j++){
+                        occurenceList.Add(objects[i]);
+                    }
+                }
+            }
+            if(nullPrefabs.Count > 0){
+                string problemString = $"{nameof(ObjectPool)} \"{this.name}\" contains {nullPrefabs.Count} Prefab(s) that are null";
+                for(int i=0; i<nullPrefabs.Count; i++){
+                    problemString += $"\n   - {nullPrefabs[i]}";
+                }
+                Debug.LogError(problemString);
+            }
+        }
+
+        public bool CanUseBoxCollForSpacing (GameObject inputGO, out BoxCollider col, out string message) {
+            var boxColliders = inputGO.GetComponents<BoxCollider>();
+            message = string.Empty;
+            col = null;
+            if(boxColliders == null || boxColliders.Length <= 0){
+                message = NO_BOXCOLLS_WARNING;
+            }else if(boxColliders.Length > 1){
+                message = TOO_MANY_BOXCOLLS_WARNING;
+            }else{
+                col = boxColliders[0];
+            }
+            return col != null;
+        }
 
         protected SplineObject InstantiatePrefabAndMeasureSize (GameObject prefab, Material material, Vector3 measureAxis, bool useBoxColliderForSpacing) {
             var newGO = Instantiate(prefab, Vector3.zero, Quaternion.identity, null);   // do i really want the zero pos and identity rotation?
@@ -16,18 +147,11 @@ namespace SplineTools{
                 var newGOMR = newGO.GetComponent<MeshRenderer>();
                 newGOMR.sharedMaterial = material;
             }
-            BoxCollider col = null; 
-            if(useBoxColliderForSpacing){
-                var boxColliders = newGO.GetComponents<BoxCollider>();
-                if(boxColliders == null || boxColliders.Length <= 0){
-                    Debug.LogError(NO_BOXCOLLS_WARNING);
-                }else if(boxColliders.Length > 1){
-                    Debug.LogError(TOO_MANY_BOXCOLLS_WARNING);
-                }else{
-                    col = boxColliders[0];
-                }
+            var canUseBoxColliderForSpacing = CanUseBoxCollForSpacing(newGO, out var col, out var msg);
+            if(useBoxColliderForSpacing && !canUseBoxColliderForSpacing){
+                Debug.LogError(msg);
             }
-            if(col != null){
+            if(useBoxColliderForSpacing && canUseBoxColliderForSpacing){
                 var worldColCenter = newGO.transform.TransformPoint(col.center);
                 var worldColSize = newGO.transform.TransformVector(col.size);
                 var worldRD = -measureAxis;
@@ -63,7 +187,34 @@ namespace SplineTools{
             }
             return new SplineObject(newGO, linearSize);
         }
+
+        [System.Serializable] 
+        public class MeshPoolObject {
+
+            [SerializeField] GameObject prefab;
+            [SerializeField, Range(1, 10)] int occurences;
+            [SerializeField] Material[] materials;
+            [SerializeField] bool useBoxColliderForSpacing;
+
+            public GameObject Prefab => prefab;
+            public int MaterialCount => materials.Length;
+            public Material Material (int i) => materials[i];
+            public int Occurences => occurences;
+            public bool UseBoxColliderForSpacing => useBoxColliderForSpacing;
+
+            public Material RandomMaterial (System.Random rng) {
+                if(MaterialCount > 0){
+                    return materials[(rng != null) ? rng.Next(0, MaterialCount) : Random.Range(0, MaterialCount)];
+                }
+                rng?.Next();
+                return null;
+            }
+
+        }
         
     }
+
+    // TODO in editor: make sure occurences is never < 1
+    // TODO in editor: when boxcolls not possible, set flag to false, gui.disable the field and add a tooltip
 
 }
