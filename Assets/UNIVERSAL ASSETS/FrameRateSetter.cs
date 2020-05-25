95585391e46f6fb0e5e5a7ffeb37b2ab8570fb4c
Max Linke <maxlinke@users.noreply.github.com>
2020-05-25 16:54:18 +0200

    better frame rate settings tool
    > removed the old, unused, terrible frameratecontrollerSCRIPT (seriously... i used a "script" suffix back then...)
    > added a new frameratesetter with a runtimemethodbuttoneditor, creating buttons for the appropriate methods

---
+++ Assets/UNIVERSAL ASSETS/FrameRateSetter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class FrameRateSetter : MonoBehaviour {
+
+    [SerializeField] int targetFrameRate = 60;
+    [SerializeField] bool vSync = false;
+
+    [RuntimeMethodButton]
+    public void SetFrameRate () {
+        Application.targetFrameRate = Mathf.Max(1, targetFrameRate);
+        targetFrameRate = Application.targetFrameRate;
+        QualitySettings.vSyncCount = (vSync ? 1 : 0);
+    }
+
+    [RuntimeMethodButton]
+    public void UnlockFrameRate () {
+        Application.targetFrameRate = -1;
+        targetFrameRate = Application.targetFrameRate;
+        QualitySettings.vSyncCount = 0;
+    }
+
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(FrameRateSetter))]
+public class FrameRateSetterEditor : RuntimeMethodButtonEditor {}
+#endif
