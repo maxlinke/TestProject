88349fd1436973932b9a40f0e7b3478f838b9ec7
Max Linke <maxlinke@users.noreply.github.com>
2020-05-25 21:02:09 +0200

    better boid diagnostics
    > showing the position in the bounding volume (needs a shorter name) with axis lines to the sides of the volume

--- Assets/bsplineobjectplacement/bird boids/SimpleBoid.cs
+++ Assets/bsplineobjectplacement/bird boids/SimpleBoid.cs
@@ -11,11 +11,11 @@ public class SimpleBoid : MonoBehaviour {
 
     [Header("Bounding Volume")]
     [SerializeField] BoxCollider boundingVolume;
-    [SerializeField] float desiredDistanceToBoundingVolume;
+    [SerializeField] float bvAvoidDistStart;
+    [SerializeField] float bvAvoidDistEnd;
 
     [Header("Grouping")]
-    [SerializeField] float localGroupRadius = 10f;
-
+    [SerializeField] float localGroupRadius;
 
     public Vector3 velocity { get; private set; }
     public float speed => velocity.magnitude;
@@ -28,17 +28,12 @@ public class SimpleBoid : MonoBehaviour {
     }
 
     void Update () {
-        if(boundingVolume == null){
+        if(!BoundingVolumeValid()){
             return;
-        }
-        if(boundingVolume.transform.rotation != Quaternion.identity){
-            boundingVolume.transform.rotation = Quaternion.identity;
-            Debug.LogWarning("bounding volume must be axis-aligned! fixed it for ya...");
         }
         var acceleration = Vector3.zero;
         GetBoundingVolumeAvoidance(out var bvInfluence, out var bvAvoidDir);
         acceleration += bvAvoidDir * bvInfluence * maxAccel;
-        Debug.DrawRay(transform.position, bvAvoidDir * bvInfluence * 10f, Color.red);
         
         velocity += acceleration * Time.deltaTime;
         velocity = velocity.normalized * Mathf.Clamp(velocity.magnitude, minSpeed, maxSpeed);
@@ -46,21 +41,57 @@ public class SimpleBoid : MonoBehaviour {
         transform.rotation = Quaternion.LookRotation(velocity, Vector3.up);
     }
 
+    bool BoundingVolumeValid () {
+        if(boundingVolume == null){
+            return false;
+        }
+        if(boundingVolume.transform.rotation != Quaternion.identity){
+            boundingVolume.transform.rotation = Quaternion.identity;
+            Debug.LogWarning("bounding volume must be axis-aligned! fixed it for ya...");
+        }
+        return true;
+    }
+
     void GetBoundingVolumeAvoidance (out float outputInfluence, out Vector3 outputDirection) {
         var localBVPos = transform.position - boundingVolume.transform.position;
         var absLocalBVPos = AbsVector3(localBVPos);
         var absExtents = AbsVector3(boundingVolume.transform.TransformVector(boundingVolume.size * 0.5f));
-        Debug.DrawRay(boundingVolume.transform.position, absLocalBVPos, Color.green);
-        Debug.DrawRay(boundingVolume.transform.position, absExtents, Color.blue);
         var absDelta = absExtents - absLocalBVPos;
-        Debug.Log(absDelta);
         var edgeDist = Mathf.Min(Mathf.Min(absDelta.x, absDelta.y), absDelta.z);
-        outputInfluence = Mathf.Clamp01(1f - (edgeDist / desiredDistanceToBoundingVolume));
+        outputInfluence = Mathf.Clamp01(1f - ((edgeDist - bvAvoidDistEnd) / (bvAvoidDistStart - bvAvoidDistEnd)));
         outputDirection = (boundingVolume.transform.TransformPoint(boundingVolume.center) - transform.position).normalized;
     }
 
     Vector3 AbsVector3 (Vector3 input) {
         return new Vector3(Mathf.Abs(input.x), Mathf.Abs(input.y), Mathf.Abs(input.z));
+    }
+
+    void OnDrawGizmosSelected () {
+        if(!BoundingVolumeValid()){
+            return;
+        }
+        var localBVPos = transform.position - boundingVolume.transform.position;
+        var worldBVSize = boundingVolume.transform.TransformVector(boundingVolume.size);
+        var xy0 = boundingVolume.transform.position + Vector3.Scale(localBVPos, new Vector3(1, 1, 0)) - Vector3.Scale(worldBVSize, new Vector3(0, 0, 0.5f));
+        var xy1 = xy0 + new Vector3(0, 0, worldBVSize.z);
+        var xz0 = boundingVolume.transform.position + Vector3.Scale(localBVPos, new Vector3(1, 0, 1)) - Vector3.Scale(worldBVSize, new Vector3(0, 0.5f, 0));
+        var xz1 = xz0 + new Vector3(0, worldBVSize.y, 0);
+        var yz0 = boundingVolume.transform.position + Vector3.Scale(localBVPos, new Vector3(0, 1, 1)) - Vector3.Scale(worldBVSize, new Vector3(0.5f, 0, 0));
+        var yz1 = yz0 + new Vector3(worldBVSize.x, 0, 0);
+        var gizmoColorCache = Gizmos.color;
+        Gizmos.color = Color.green;
+        LineWithCubeAtEnd(xy0);
+        LineWithCubeAtEnd(xy1);
+        LineWithCubeAtEnd(xz0);
+        LineWithCubeAtEnd(xz1);
+        LineWithCubeAtEnd(yz0);
+        LineWithCubeAtEnd(yz1);
+        Gizmos.color = gizmoColorCache;
+
+        void LineWithCubeAtEnd (Vector3 endPoint) {
+            Gizmos.DrawLine(transform.position, endPoint);
+            Gizmos.DrawCube(endPoint, Vector3.one * 0.1f);
+        }
     }
 	
 }
