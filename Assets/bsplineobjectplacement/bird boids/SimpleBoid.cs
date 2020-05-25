6835c3e2ea2fbee6791eb62a9fed84ccfc18100e
Max Linke <maxlinke@users.noreply.github.com>
2020-05-25 16:26:49 +0200

    boids: starting simple
    > added arrowhead model from shmup
    > start of simpler boid script
    > other things first tho

---
+++ Assets/bsplineobjectplacement/bird boids/SimpleBoid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleBoid : MonoBehaviour {
+
+    [Header("Movement Space")]
+    [SerializeField] BoxCollider boundingVolume;
+    // [SerializeField] 
+
+    [Header("Grouping")]
+    [SerializeField] float localGroupRadius = 10f;
+
+
+    public Vector3 velocity { get; private set; }
+    public Vector3 position => transform.position;
+
+    static List<SimpleBoid> boids;
+
+    void Start () {
+
+    }
+
+    void Update () {
+        if(boundingVolume == null){
+            return;
+        }
+        if(boundingVolume.transform.rotation != Quaternion.identity){
+            boundingVolume.transform.rotation = Quaternion.identity;
+            Debug.LogWarning("bounding volume must be axis-aligned! fixed it for ya...");
+        }
+    }
+	
+}
