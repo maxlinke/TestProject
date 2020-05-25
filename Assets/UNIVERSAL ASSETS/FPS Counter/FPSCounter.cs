f292249d978f0d32a53ced2b2da6496316ee859e
Max Linke <maxlinke@users.noreply.github.com>
2020-05-25 17:06:46 +0200

    some more updates of old scripts
    > removed the "script"-suffix from the fps counter script
    > made the variables private and serialized (they were still just public...)
    > fixed a potential issue because the smoothing could be set to 0, which would have caused indexoutofrangeexceptions

---
+++ Assets/UNIVERSAL ASSETS/FPS Counter/FPSCounter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour {
+
+	[SerializeField] Text textField = default;
+	[SerializeField, Range(0, 100)] int smoothing = 10;
+	[SerializeField] bool updateEveryFrame = true;
+
+	float[] timeDeltas;
+	int rollingIndex;
+
+	void Start () {
+		timeDeltas = new float[smoothing+1];
+	}
+	
+	void Update () {
+		RecordCurrentValue();
+		bool updateNow = updateEveryFrame || rollingIndex == 0;
+		if(updateNow){
+			UpdateDisplay();
+		}
+	}
+
+	private void RecordCurrentValue(){
+		timeDeltas[rollingIndex] = Time.unscaledDeltaTime;
+		rollingIndex = (rollingIndex + 1) % timeDeltas.Length;
+	}
+
+	private void UpdateDisplay(){
+		float sum = 0f;
+		for(int i=0; i<timeDeltas.Length; i++){
+			sum += timeDeltas[i];
+		}
+		float average = sum / timeDeltas.Length;
+		textField.text = ((int)(1f / average)).ToString();
+	}
+}
