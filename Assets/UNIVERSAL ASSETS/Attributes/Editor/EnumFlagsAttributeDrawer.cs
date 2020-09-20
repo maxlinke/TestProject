c71ddb4b90620400b9c18cfcaaef55f70386cd0f
Max Linke <maxlinke@users.noreply.github.com>
2020-09-21 01:30:31 +0200

    imported my attributes from the "dawn" project

---
+++ Assets/UNIVERSAL ASSETS/Attributes/Editor/EnumFlagsAttributeDrawer.cs
@@ -0,0 +1,23 @@
+﻿// source: https://answers.unity.com/questions/486694/default-editor-enum-as-flags-.html
+
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+public class EnumFlagsAttributeDrawer : PropertyDrawer {
+
+    public override float GetPropertyHeight (SerializedProperty property, GUIContent label) {
+        return EditorGUIUtility.singleLineHeight;
+    }
+
+    public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+        EditorGUI.showMixedValue = property.hasMultipleDifferentValues;
+        EditorGUI.BeginChangeCheck();
+        var origValue = property.intValue;
+        var newValue = EditorGUI.MaskField(position, label, origValue, property.enumDisplayNames);
+        if(EditorGUI.EndChangeCheck()){
+            property.intValue = newValue;
+        }
+    }
+
+}
--- Assets/UNIVERSAL ASSETS/Attributes/RuntimeMethodButtonAttribute.cs
+++ Assets/UNIVERSAL ASSETS/Attributes/RuntimeMethodButtonAttribute.cs
@@ -1,10 +1,19 @@
-﻿using UnityEngine;
-using System.Reflection;
-using System.Collections.Generic;
-
+﻿using System;
 #if UNITY_EDITOR
 using UnityEditor;
+using UnityEngine;
+using System.Reflection;
+using System.Collections.Generic;
+#endif
 
+[AttributeUsage(AttributeTargets.Method)]
+public class RuntimeMethodButtonAttribute : Attribute { }
+
+// If this editor is placed in a new script in an "Editor" folder, scripts can't use this editor
+// locally anymore. Since the point of this is to enable scripts to have buttons without needing
+// to set up an actual custom editor, it is within these #if-defines.
+
+#if UNITY_EDITOR
 public abstract class RuntimeMethodButtonEditor : Editor {
 
     MethodInfo[] methods;
@@ -22,6 +31,9 @@ public abstract class RuntimeMethodButtonEditor : Editor {
         if(EditorApplication.isPlaying){
             if(methods == null){
                 FindAppropriateMethods();
+            }
+            if(methods.Length > 0){
+                GUILayout.Space(10f);
             }
             foreach(var method in methods){
                 if(GUILayout.Button(method.Name)){
@@ -46,3 +58,4 @@ public abstract class RuntimeMethodButtonEditor : Editor {
 #else
 public abstract class RuntimeMethodButtonEditor { }
 #endif
+
