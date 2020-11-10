52ad9a137a2b23f5233210d6c5d966a1c627620c
Max Linke <maxlinke@users.noreply.github.com>
2020-11-10 12:11:05 +0100

    removed unused material, more experiments with jiggle bones

---
+++ Assets/UNIVERSAL ASSETS/SuperSimpleXYZMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class SuperSimpleXYZMove : MonoBehaviour {
+
+    [SerializeField] float maxSpeed = 10f;
+    [SerializeField] float maxAccel = 10f;
+
+    Vector3 velocity;
+
+    void Start () {
+        velocity = Vector3.zero;
+    }
+
+    void Update () {
+        Vector3 input = Vector3.zero;
+        input += Dir(KeyCode.W, Vector3.forward);
+        input -= Dir(KeyCode.S, Vector3.forward);
+        input += Dir(KeyCode.D, Vector3.right);
+        input -= Dir(KeyCode.A, Vector3.right);
+        input += Dir(KeyCode.E, Vector3.up);
+        input -= Dir(KeyCode.Q, Vector3.up);
+        if(input.sqrMagnitude > 1f){
+            input = input.normalized;
+        }
+        Vector3 accel = Vector3.zero;
+        if(input.sqrMagnitude > 0.01f){
+            accel = input * maxAccel;
+        }else{
+            accel = -velocity.normalized * Mathf.Min(maxAccel, velocity.magnitude / Time.deltaTime);
+        }
+        velocity += accel * Time.deltaTime;
+        if(velocity.sqrMagnitude > (maxSpeed * maxSpeed)){
+            velocity = velocity.normalized * maxSpeed;
+        }
+        transform.position += velocity * Time.deltaTime;
+
+        Vector3 Dir (KeyCode keyCode, Vector3 direction) {
+            return Input.GetKey(keyCode) ? direction : Vector3.zero;
+        }
+    }
+
+
+	
+}
---
+++ Assets/animations1/JiggleBoneCustom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JiggleBoneCustom : MonoBehaviour {
+
+    [Header("Debug")]
+    [SerializeField] UnityEngine.UI.Text debugText = default;
+
+    [Header("Translation")]
+    [SerializeField] AnimationCurve springForce = AnimationCurve.Linear(0,0,1,1);
+    [SerializeField] float maxDistance = 1f;
+    [SerializeField] float maxForce = 1f;
+    [SerializeField, Range(0, 1)] float gravityInfluence = 1f;
+
+    Vector3 initLocalPosition;
+    Vector3 lastWorldPosition;
+
+    void Start () {
+        initLocalPosition = transform.localPosition;
+        lastWorldPosition = transform.position;
+    }
+
+    void LateUpdate () {
+        transform.position = lastWorldPosition;
+        var targetPosition = transform.parent.TransformPoint(initLocalPosition);
+        lastWorldPosition = transform.position;
+        Debug.DrawRay(targetPosition, Vector3.up, Color.magenta, Time.deltaTime, false);
+        // if(debugText != null){
+        //     debugText.text = $"dt: {Time.deltaTime:F3}\nfps:{(1f/Time.deltaTime):F1}\nlrp:{lerp:F3}";
+        // }
+    }
+	
+}
